04116a029a7c77ca17e75f359c606665bdf649c5
linyuhsuan <vickey3703@gamil.com>
2020-06-11 19:15:40 +0800

    change

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,6 +4,6 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  APIPATH:'"http://vue-course-api.hexschool.io"',
+  APIPATH:'"https://vue-course-api.hexschool.io"',
   CUSTOMPATH:'"shelly"',
 })
